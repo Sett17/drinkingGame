3e9967fc25fcfa787df394831d62944d3b33c036
Sett17 <raik.rohde@gmail.com>
2021-03-20 22:20:49 +0100

    Build and Deploy from build.py at 20.03.21 22:20

--- live.py
+++
@@ -1,6 +0,0 @@
-from livereload import Server, shell
-print("Only use in dev")
-server = Server()
-server.watch('./dev')
-server.setHeader('Access-Control-Allow-Origin', '*')
-server.serve(root='./dev')
